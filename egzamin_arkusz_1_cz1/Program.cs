a7344783646420a4a30a3349d379e528e28002d0
marcin.bulanda <marcin.bulanda@S50-09>
2024-09-20 09:28:51 +0200

    Dodaj pliki projektów.

---
+++ egzamin_arkusz_1_cz1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace egzamin_arkusz_1_cz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
