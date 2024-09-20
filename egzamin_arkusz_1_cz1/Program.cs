727bf68daa9bbc1800589dfc1efc044c1deab00d
PallFox <marcin.bulanda@op.pl>
2024-09-20 09:54:28 +0200

    Dodanie klasy Song. Dodanie funkcjonalnosci czytania pliku.

--- egzamin_arkusz_1_cz1/Program.cs
+++ egzamin_arkusz_1_cz1/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Song> list = new List<Song>();
+            StreamReader sr = new StreamReader("C:\\Users\\marcin.bulanda\\Desktop\\egzamin\\Data.txt");
+            string line;
+            while((line=sr.ReadLine()) != null)
+            {
+                try
+                {
+                    Song song = new Song();
+                    song.Artist = line;
+                    song.Album = sr.ReadLine();
+                    song.songsNumber = int.Parse(sr.ReadLine());
+                    song.Year = int.Parse(sr.ReadLine());
+                    song.DownloadNumber = int.Parse(sr.ReadLine());
+                    list.Add(song);
+                }catch { }
+            }
+            sr.Close();
+
+            Console.WriteLine(list);
         }
     }
 }
---
+++ egzamin_arkusz_1_cz1/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace egzamin_arkusz_1_cz1
+{
+    public class Song
+    {
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public int songsNumber { get; set; }
+        public int Year { get; set; }
+        public int DownloadNumber { get; set; }
+    }
+}
